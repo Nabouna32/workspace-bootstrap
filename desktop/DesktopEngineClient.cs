90f13471914dea8bda117b548013deb818b8157c
Rupy <nabouna@gmx.fr>
2026-09-22 12:23:59 +0200

    refactor: normalize desktop namespaces

--- desktop/DesktopEngineClient.cs
+++ desktop/DesktopEngineClient.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 
-namespace BounaDevEnvironment;
+namespace WorkspaceBootstrap.Desktop;
 
 public sealed class DesktopEngineClient
 {
