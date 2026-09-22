fd0cb546690929211aeabbba51dc0703f48073cc
Rupy <nabouna@gmx.fr>
2026-09-23 01:44:52 +0200

    fix: disambiguate WinUI Application.Current

--- src/WorkspaceControl.Desktop/ProfilesPage.xaml.cs
+++ src/WorkspaceControl.Desktop/ProfilesPage.xaml.cs
@@ -7,6 +7,6 @@ public sealed partial class ProfilesPage : Microsoft.UI.Xaml.Controls.Page
     public ProfilesPage()
     {
         InitializeComponent();
-        DataContext = ((App)Application.Current).ViewModel;
+        DataContext = ((App)Microsoft.UI.Xaml.Application.Current).ViewModel;
     }
 }
