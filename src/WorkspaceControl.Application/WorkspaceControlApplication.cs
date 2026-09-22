09d44912706d1f271c62aa0d305619d8dfd7121e
Rupy <nabouna@gmx.fr>
2026-09-23 00:19:25 +0200

    feat: orchestrate software inventory in application layer

--- src/WorkspaceControl.Application/WorkspaceControlApplication.cs
+++ src/WorkspaceControl.Application/WorkspaceControlApplication.cs
@@ -8,6 +8,7 @@ public sealed class WorkspaceControlApplication : IWorkspaceControlApplication
     private readonly IProvisioningService _provisioning;
     private readonly IInventoryService _inventory;
     private readonly ISoftwareInventoryService _softwareInventory;
+    private readonly ISoftwareInventoryService _softwareInventory;
     private readonly IWindowsAdministrationService _windows;
     private readonly IOptimizationService _optimization;
 
@@ -20,6 +21,7 @@ public sealed class WorkspaceControlApplication : IWorkspaceControlApplication
     {
         _provisioning = provisioning ?? throw new ArgumentNullException(nameof(provisioning));
         _inventory = inventory ?? throw new ArgumentNullException(nameof(inventory));
+        _softwareInventory = softwareInventory ?? throw new ArgumentNullException(nameof(softwareInventory));
         _softwareInventory = softwareInventory ?? throw new ArgumentNullException(nameof(softwareInventory));
         _windows = windows ?? throw new ArgumentNullException(nameof(windows));
         _optimization = optimization ?? throw new ArgumentNullException(nameof(optimization));
@@ -64,6 +66,9 @@ public sealed class WorkspaceControlApplication : IWorkspaceControlApplication
 
     public Task<InventorySnapshot> GetInventoryAsync(CancellationToken cancellationToken = default) =>
         _inventory.ScanAsync(cancellationToken);
+
+    public Task<SoftwareInventorySnapshot> GetSoftwareInventoryAsync(CancellationToken cancellationToken = default) =>
+        _softwareInventory.ScanAsync(cancellationToken);
 
     public Task<SoftwareInventorySnapshot> GetSoftwareInventoryAsync(CancellationToken cancellationToken = default) =>
         _softwareInventory.ScanAsync(cancellationToken);
