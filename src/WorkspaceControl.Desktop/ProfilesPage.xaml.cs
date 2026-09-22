eb509d19ad738b1068139d11834fb577452d7d9a
Rupy <nabouna@gmx.fr>
2026-09-23 00:23:58 +0200

    feat: add Workspace Control page

---
+++ src/WorkspaceControl.Desktop/ProfilesPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace WorkspaceControl.Desktop;
+
+public sealed partial class ProfilesPage : Microsoft.UI.Xaml.Controls.Page
+{
+    public ProfilesPage()
+    {
+        InitializeComponent();
+        DataContext = ((App)Application.Current).ViewModel;
+    }
+}
