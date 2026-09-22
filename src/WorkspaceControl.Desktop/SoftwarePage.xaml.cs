9fc86b1802630e4b733a50747b6677c2e7500adc
Rupy <nabouna@gmx.fr>
2026-09-23 00:23:34 +0200

    feat: add Workspace Control page

---
+++ src/WorkspaceControl.Desktop/SoftwarePage.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WorkspaceControl.Desktop;
+
+public sealed partial class SoftwarePage : Page
+{
+    public SoftwarePage()
+    {
+        InitializeComponent();
+        DataContext = ((App)Application.Current).ViewModel;
+    }
+
+    private async void Refresh_Click(object sender, RoutedEventArgs e) =>
+        await ((WorkspaceControlViewModel)DataContext).RefreshAsync();
+}
