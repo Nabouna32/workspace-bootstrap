45e1cca05a426d2bb6a01f222b0a1e21e051b1c9
Rupy <nabouna@gmx.fr>
2026-09-22 23:38:32 +0200

    feat: add Workspace Control application layer

---
+++ src/WorkspaceControl.Application/IWorkspaceControlApplication.cs
@@ -0,0 +1,22 @@
+using WorkspaceBootstrap;
+using WorkspaceControl.Domain;
+
+namespace WorkspaceControl.Application;
+
+public interface IWorkspaceControlApplication
+{
+    IReadOnlyList<ProfileManifest> GetProfiles();
+    Task<object> GetPlanAsync(string profileId, CancellationToken cancellationToken = default);
+    string StartProvisioning(string profileId, bool cacheOnly = false);
+    Task RunProvisioningAsync(string operationId, bool cacheOnly = false, CancellationToken cancellationToken = default);
+    ProvisioningOperation? GetProvisioningStatus(string operationId);
+    string ResumeProvisioning(string operationId, bool cacheOnly = false);
+    IReadOnlyList<ProvisioningOperation> GetProvisioningHistory();
+    ProvisioningOperation? GetProvisioningRecovery();
+    ProvisioningOperation? GetProvisioningDetail(string operationId);
+    Task<InventorySnapshot> GetInventoryAsync(CancellationToken cancellationToken = default);
+    BaselineSnapshot GetBaseline();
+    IReadOnlyList<OptimizationPlanItem> GetSafeOptimizationPlan();
+    IReadOnlyList<string> ApplySafeOptimization();
+    IReadOnlyList<string> RollbackOptimization();
+}
