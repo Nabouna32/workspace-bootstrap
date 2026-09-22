6ef3997191d840dc80fd430744ec3ccc596caf76
Rupy <nabouna@gmx.fr>
2026-09-22 23:50:25 +0200

    fix: disambiguate WinUI application base type

--- src/WorkspaceControl.Desktop/App.xaml.cs
+++ src/WorkspaceControl.Desktop/App.xaml.cs
@@ -2,7 +2,7 @@ using Microsoft.UI.Xaml;
 
 namespace WorkspaceControl.Desktop;
 
-public partial class App : Application
+public partial class App : Microsoft.UI.Xaml.Application
 {
     private Window? _window;
 
