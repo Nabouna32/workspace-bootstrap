5782a4838c1b8b7466adfe4f30b8c012cdfae982
Rupy <nabouna@gmx.fr>
2026-09-23 02:19:18 +0200

    fix: attach shell bindings to root grid

--- src/WorkspaceControl.Desktop/MainWindow.xaml.cs
+++ src/WorkspaceControl.Desktop/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = ((App)Microsoft.UI.Xaml.Application.Current).ViewModel;
+        RootGrid.DataContext = ((App)Microsoft.UI.Xaml.Application.Current).ViewModel;
         NavView.SelectedItem = NavView.MenuItems[0];
         Activated += MainWindow_Activated;
     }
